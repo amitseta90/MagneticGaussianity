Corrected QU Faraday rotation

observables.py
@@ -1,13 +1,8 @@
 import numpy as np 
 import scipy.special as sp
-import nifty8 as ift
-
-def cartesian(shape, direction):
-    domain = ift.makeDomain(ift.RGSpace(shape))
-    return ift.IntegrationOperator(domain, spaces=direction)
 
 
-def calculate_observables(B_perp, B_par, pol_angle, n_th, n_cr, do_rm=False, do_syncI = False, do_syncQU= False, faraday_rotate=False, spectral_index = 3., lambda_square=1., direction=0):
+def calculate_observables(B_par, B_perp,  pol_angle, n_th, n_cr, do_rm=False, do_syncI = False, do_syncQU= False, faraday_rotate=False, spectral_index = 3., lambda_square=1., direction=0, rm_prev=None):
     
     if not do_rm and not do_syncI and not do_syncQU:
         raise ValueError("Nothing to do")
@@ -26,39 +21,36 @@ def calculate_observables(B_perp, B_par, pol_angle, n_th, n_cr, do_rm=False, do_
     
     results = [None, None, None, None] # RM, I, Q, U
     
-    domain = None
+    shape = None
     for f in (B_perp, B_par, n_cr, n_th): 
         if isinstance(f, np.ndarray):
-            if domain is None:
-                domain = ift.makeDomain(ift.RGSpace(f.shape))
+            if shape is None:
+                shape = f.shape
             else: 
-                if domain.shape != f.shape: # type: ignore
+                if shape != f.shape: # type: ignore
                     raise ValueError("Inconsistent shapes")
         elif not (isinstance(f, float) or f is None):
             raise TypeError("Fields must be numpy arrays or a single float, {} detected".format(type(f))) 
              
-    if domain is None:
+    if shape is None:
         raise ValueError("At last one np.ndarray must be provided")
-    
-
-    integrator = cartesian(domain.shape, direction) # type: ignore
     
     lsd = None
     
     if do_rm or faraday_rotate: 
         fs = faraday_source(B_par, n_th)
         if do_rm:
-            results[0] = integrator(ift.makeField(domain, fs)).val
+            results[0] = np.sum(fs, axis=direction)
         if faraday_rotate: 
-            lsd = lambda_square*faraday_depth(fs, direction)  
+            lsd = lambda_square*faraday_depth(fs, direction, rm_prev)  
 
     if do_syncI or do_syncQU:
         si, sq, su = synchrotron_emissivities(B_perp, n_cr, pol_angle, spectral_index, lsd) 
         if do_syncI:
-            results[1] = integrator(si).val
+            results[1] = np.sum(si, axis=direction)
         if do_syncQU:
-            results[2] = integrator(sq).val
-            results[3] = integrator(su).val
+            results[2] = np.sum(sq, axis=direction)
+            results[3] = np.sum(su, axis=direction)
             
     return tuple(results)
     
@@ -83,9 +75,11 @@ def faraday_source(B_par, n_th):
     return B_par*n_th
     
 
-def faraday_depth(fs, direction):
+def faraday_depth(fs, direction, prev_rm):
     dr = 1.
-    return np.cumsum(fs*dr, axis=direction)
+    fd = np.cumsum(np.concatenate((np.expand_dims(prev_rm, direction), fs*dr), axis=direction), axis=direction)
+    fd = np.delete(fd, 0, direction)
+    return fd
     
     
     